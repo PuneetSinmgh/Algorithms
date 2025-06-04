3b407aa353b005e67a31821c9395995cd5747d50
Puneet Singh <singh.puneet0401@gmail.com>
2025-06-03 23:17:42 -0700

    Added greedy problems

---
+++ GreedyAlgorithm/src/my/com/app/JumpGames.java
@@ -0,0 +1,78 @@
+package src.my.com.app;
+
+import java.util.*;
+
+class Solution {
+
+    int[] dp;
+    public boolean canJump(int[] nums) {
+     /*     
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+        
+       
+        return jumps(nums,0);
+    }
+    public boolean jumps (int[] nums , int idx){
+     
+        if(idx == nums.length-1){
+            return true;
+        }
+        
+        int maxjump = Math.min(idx+nums[idx], nums.length-1);
+        
+        for(int i=idx+1 ; i<= maxjump;i++){
+            
+            if(jumps(nums,i))
+                return true;
+            
+        }
+        return false; */
+
+    /*    dp = new int[nums.length];
+        
+        Arrays.fill(nums, -1);
+        dp[nums.length - 1] = 1;
+        
+        return reachLast(nums, 0 );
+
+    }
+
+    public boolean reachLast( int[] nums, int idx){
+        if (  dp[idx] != -1 )
+            return dp[idx] == 1 ? true : false;
+
+        int maxJump = Math.min( idx + nums[idx], nums.length-1 );
+
+        for (int i= idx+1 ; i<= maxJump ; i++) { // since maximum number of step is val, so we can take any step between 1 to val
+            if (reachLast(nums, i )){
+                dp[idx] = 1;
+                return true;
+            }
+        }
+        
+        dp[idx] = 0;
+        return false;
+
+    */
+
+    // Greedy solution 
+
+
+        int last = nums.length-1;
+
+        for ( int i = nums.length-1 ; i >=0 ; i-- ){
+
+            if ( i +nums[i] >= last ){
+                last = i;
+            }
+        }
+
+        return last == 0;
+    }
+}
---
+++ GreedyAlgorithm/src/my/com/app/MinBoatsToSavePeople.java
@@ -0,0 +1,29 @@
+package src.my.com.app;
+
+import java.util.*;
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        // since all the weight of people are less than limit 
+        // sort the array in ascending order , then take two lowest value sum less than equal to limit.
+        if ( people.length == 1)
+            return 1;
+
+        Arrays.sort(people);
+
+        int i=0, j =people.length-1, count =0;
+        while ( i<=j ){
+            if (people[i] + people[j] <= limit ){
+                i++;
+                j--;
+                
+            } else {
+                j--;
+            }
+            count++;
+        }
+        
+        return count;
+    }
+}
