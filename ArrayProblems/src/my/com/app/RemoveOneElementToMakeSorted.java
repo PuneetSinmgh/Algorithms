4b003185e18bd248b682ca1cc9f679612f1b6e39
Puneet Singh <singh.puneet0401@gmail.com>
2025-06-15 13:46:01 -0700

    Adding DP problems

---
+++ ArrayProblems/src/my/com/app/RemoveOneElementToMakeSorted.java
@@ -0,0 +1,55 @@
+package src.my.com.app;
+
+import java.util.*;
+// Approach: Iterate through the array from the end to the beginning.
+// If we find a pair of elements where the current element is not greater than the previous one, we have two options:
+// 1. Remove the current element and check if the rest of the array is strictly increasing.
+// 2. Remove the previous element and check if the rest of the array is strictly increasing.
+// We can use a boolean flag to track if we have already removed an element.
+// If we find a second pair that violates the strictly increasing condition after removing one element, we return false.
+
+class Solution {
+    public boolean canBeIncreasing(int[] nums) {
+         
+        int i = nums.length-1;
+        boolean removed = false ;
+        while ( i>0 ){
+
+            if ( nums[i] > nums[i-1]){
+                i--;
+            } else {
+                if ( removed ){
+                    return false;
+                }else { // first encounter
+                    removed = true;
+                    if ( i>1 && nums[i] > nums[i-2] )
+                        i-=2;
+                    else{
+                        if ( i==1 && nums[i] <= nums[i-1]){
+                            return true;
+                        }
+
+                        if ( i == nums.length-1 ){
+                            if (nums[i-1] >= nums[i-2]){
+                                i-=1;
+                                continue;
+                            } else {
+                                return false;
+                            }
+                        }
+
+                        if ( nums[i+1] > nums[i-1] ){
+                            i-=1;
+                            continue;
+                        }
+
+                        return false;
+                    }
+                        
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ DynamicPrograming/src/my/com/app/knapsack/PartitionEqualSubsetSum.java
@@ -0,0 +1,39 @@
+package my.com.app.knapsack;
+import java.util.*;
+// Approach: Use dynamic programming to solve the subset sum problem.
+// We will create a 2D boolean array dp where dp[i][j] will be true if a subset of the first i elements can sum up to j.
+// We will iterate through the array and update the dp array based on whether we include or exclude the current element.
+
+class Solution {
+    
+    Boolean[][] dp ;
+    
+    public boolean canPartition(int[] nums) {
+    
+        int sum = Arrays.stream(nums).sum();
+        // since we cannot choose fractional elements, thus id total sum is odd , it can't be devided in equally
+        if ( sum%2 != 0)
+            return false;
+
+        sum /=2;
+        dp = new Boolean[nums.length][sum+1]; // using memoization 
+
+        return subsetSum(nums,0 , sum);
+
+    }
+
+    public boolean subsetSum(int[] nums, int idx, int target){
+
+        if (target == 0)
+            return true;
+        
+        if (target<0 || idx == nums.length)
+            return false;
+
+        if (dp[idx][target] != null)   
+            return dp[idx][target];
+
+        dp[idx][target] = subsetSum(nums, idx+1 , target-nums[idx]) || subsetSum(nums, idx+1 , target);
+        return dp[idx][target];
+    }
+}
