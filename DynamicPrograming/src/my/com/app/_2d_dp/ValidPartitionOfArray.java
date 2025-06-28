bbb0573d073d5b897e7b5a1cd6ab04db99e4ac62
Puneet Singh <singh.puneet0401@gmail.com>
2025-06-27 17:47:29 -0700

    Added DP problem

---
+++ DynamicPrograming/src/my/com/app/_2d_dp/ValidPartitionOfArray.java
@@ -0,0 +1,84 @@
+package my.com.app._2d_dp;
+
+import java.util.*;
+
+class Solution {
+    Boolean[][] dp ;
+    
+    Map<Integer, Boolean> memo = new HashMap<>();
+
+    public boolean validPartition(int[] nums) {
+      /*
+        int n = nums.length;
+        if (n < 2)
+            return false;
+        
+        dp = new Boolean[n][n];
+
+        return recursivePartition(nums, 0, nums.length-1);
+        */
+
+        int n = nums.length;
+        memo.put(-1, true);
+
+        return prefixIsValid( nums, n-1);
+    }
+
+    boolean prefixIsValid(int[] nums, int i) {
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+        boolean ans = false;
+
+        // Check 3 possibilities
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            ans |= prefixIsValid(nums, i - 2);
+        }
+        if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
+            ans |= prefixIsValid(nums, i - 3);
+        }
+        if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i - 1] == nums[i - 2] + 1) {
+            ans |= prefixIsValid(nums, i - 3);
+        }
+        memo.put(i, ans);
+        return ans;
+    }
+
+    public boolean recursivePartition( int[] nums , int s , int e ){
+
+        if ( dp[s][e] != null)
+            return dp[s][e];
+
+        if( e-s +1 ==3){
+            if ( (nums[s] == nums[s+1] && nums[s+1] == nums[s+2]) || 
+                ( nums[s] +1 == nums[s+1] && nums[s+1] +1 == nums[s+2] )){
+                    dp[s][e] =  true;
+                    return dp[s][e];
+                } else{
+                    dp[s][e] =  false;
+                    return dp[s][e];
+                }
+        } else if (e-s+1 == 2){
+            if ( nums[s] == nums[e]){
+                dp[s][e] =  true;
+                return dp[s][e];
+            } else{
+                    dp[s][e] =  false;
+                    return dp[s][e];
+            }
+        } else if (e-s+1 < 2 ){
+            dp[s][e] =  false;
+            return dp[s][e];
+        }
+        
+        for ( int i =s ; i< e ;i++){
+            if  (recursivePartition(nums, s, i) && recursivePartition(nums, i+1, e)){
+                dp[s][e] =  true;
+                return dp[s][e];
+            }
+        }
+
+        dp[s][e] =  false;
+        return dp[s][e];
+    }
+}
