546ba664137931e4e637233d01fdeb05bea87d5b
Puneet Singh <singh.puneet0401@gmail.com>
2025-07-26 17:29:01 -0700

    added two sum prob

--- HashTables/src/my/com/hashmap/TwoSumDataStructure.java
+++ HashTables/src/my/com/hashmap/TwoSumDataStructure.java
@@ -1,26 +1,33 @@
 package my.com.hashmap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class TwoSumDataStructure {
     
-     Map<Integer, Integer> hm;
-    
+    //Map<Integer, Integer> hm;
+    List<Integer> stream;
+    boolean isSorted;
     public TwoSumDataStructure() {
 
-        hm  = new TreeMap<>();    
-    
+        //hm  = new TreeMap<>();    
+        stream = new ArrayList();
+        isSorted = false;
     }
     
     public void add(int number) {
 
-        hm.put( number, hm.getOrDefault(number, 0 )+1);
+        //hm.put( number, hm.getOrDefault(number, 0 )+1);
+        stream.add(number);
+        isSorted = false;
     
     }
     
     public boolean find(int value) {
-        
+      /*  
         for (int i : hm.keySet()){
             if ( hm.containsKey( value - i)){
                 if ( i == value-i && hm.get( i ) == 1){
@@ -30,6 +37,25 @@ public class TwoSumDataStructure {
                 return true;
             }
         }
+        return false;
+        */
+
+        if ( !isSorted ){
+            Collections.sort(stream);
+        }
+        
+        int i =0 , j = stream.size()-1;
+        while ( i< j) {
+            if ( stream.get(i) + stream.get(j) == value ){
+                return true;
+            } else if (stream.get(i) + stream.get(j) < value ) {
+                i++;
+            } else {
+                j--;
+            }
+
+        }
+
         return false;
     }
 }
